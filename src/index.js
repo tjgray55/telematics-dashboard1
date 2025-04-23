c80c742a650b6f72d4b0f1d498a15d5a10e88069
Gray <tjgray@walshgroup.com>
2025-04-23 11:14:20 -0500

    Initial commit: Telematics dashboard

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import TelematicsDashboard from './components/TelematicsDashboard';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <TelematicsDashboard />
+  </React.StrictMode>
+);
